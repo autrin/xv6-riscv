f10ba83aaea7bd8179d017d551180151eb69dced
Autrin <ahakimi@iastate.edu>
2024-10-24 10:49:42 -0500

    added a process runtime PCB

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int runtime;                 // Process runtime is the time spent on the CPU
 };
