e7970e8645e87d35ab0a1817709cf8d1cde1a8f1
Autrin <autrinhakimi@gmail.com>
2024-10-27 12:40:09 -0500

    created another test for I/O bound processes and chenged the N to 1000

--- user/schedtestc.c
+++ user/schedtestc.c
@@ -2,20 +2,16 @@
 #include "user/user.h"
 #include "kernel/syscall.h"
 
-#define N  1000000000
-/*
-Use fork to create two new child processes and set their stride values to 4 and 12. Both processes
-then enter long CPU-bursts that lasts several ticks, for example, spinning in a long loop.
-We would expect the child with stride of 4 to get about 3 times as much runtime. Call the
-getruntime() system call for each process and print the results to verify this.
- */
+#define N  1000
+
 void
-fork_stride_test(void)
+test1(void)
 {
-    int pid1, pid2, i;
+    int pid1, pid2;
+    uint64 i;
     uint runtime1, runtime2;
     
-    printf("fork_stride_test\n");
+    printf("test1\n");
 
     // first child process
     pid1 = fork();
@@ -40,12 +36,51 @@ fork_stride_test(void)
                               // unnecessary waiting calls by the child processes
         wait(0); // wait for child 1
         wait(0); // wait for child 2
-        printf("fork_stride_test() completed\n");
+        printf("test1() completed\n");
+    }
+}
+
+void
+test2(void)
+{
+    int pid1, pid2;
+    uint64 i;
+    uint runtime1, runtime2;
+    
+    printf("test2\n");
+
+    // first child process
+    pid1 = fork();
+    if(pid1 == 0){
+        stride(getpid(), 4); // child 1 with stride 4
+        for(i = 0; i < N; i++){} // cpu burst
+        runtime1 = getruntime(pid1);
+        printf("Child 1 finished with runtime: %d\n", runtime1);
+        exit(0);
+    }
+    // second child process
+    pid2 = fork();
+    if(pid2 == 0){
+        stride(getpid(), 12); // child 2 with stride 12
+        for(i = 0; i < N; i++){
+            sleep(1); // put the process into block state
+        }
+        runtime2 = getruntime(pid2);
+        printf("Child 2 finished with runtime: %d\n", runtime2);
+        exit(0);
+    }
+
+    if(pid1 > 0 && pid2 > 0){ // ensures that we are in the parent process to avoid 
+                              // unnecessary waiting calls by the child processes
+        wait(0); // wait for child 1
+        wait(0); // wait for child 2
+        printf("test2() completed\n");
     }
 }
 
 int 
 main(void) {
-    fork_stride_test();
+    test1();
+    test2();
     exit(0);
 }
