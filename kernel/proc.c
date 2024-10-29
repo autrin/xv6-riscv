f61861f33af820ce2f698423f10ed531c4cfc737
Autrin <autrinhakimi@gmail.com>
2024-10-28 22:32:27 -0500

    new line

--- kernel/proc.c
+++ kernel/proc.c
@@ -469,7 +469,8 @@ userinit(void)
 
   p = allocproc();
   initproc = p;
-  pintf("passed line 472");
+  printf("passed line 472\n");
+  
   // allocate one user page and copy initcode's instructions
   // and data into it.
   uvmfirst(p->pagetable, initcode, sizeof(initcode));
